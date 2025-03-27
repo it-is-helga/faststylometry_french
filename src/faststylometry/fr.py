c695793b351f5b6f224078aaf96f6f6bac23d5ee
it-is-helga <oshablykina@gmail.com>
2025-03-27 16:21:59 +0300

    Fr tokenizer

--- src/faststylometry/fr.py
+++ src/faststylometry/fr.py
@@ -27,13 +27,28 @@ SOFTWARE.
 
 '''
 
-__version__ = "1.0.4"
+import re
+
+is_number_pattern = re.compile(r'.*\d.*')
+
+re_words = re.compile(r"\w+")
+
+# pronouns
+with open("stopwords_fr.txt", "r", encoding="utf-8") as file:
+    stopwords_fr = {line.strip() for line in file}
 
 
-from faststylometry.corpus import Corpus
-from faststylometry.util import load_corpus_from_folder
-from faststylometry.en import tokenise_remove_pronouns_en
-from faststylometry.fr import tokenise_remove_pronouns_fr
-from faststylometry.burrows_delta import calculate_burrows_delta
-from faststylometry.probability import predict_proba, calibrate, get_calibration_curve
-from faststylometry.examples import download_examples
+def tokenise_remove_pronouns_fr(text: str) -> list:
+    """
+    Tokenise a sentence according to French rules, and remove all pronouns.
+    Remove all apostrophes since words like don't etc can be written in nonstandard ways.
+
+    :param text: the original sentence.
+    :return: all non-pronoun tokens.
+    """
+    text_normalised = re.sub("['’]", "", text.lower())
+    tokens = [tok for tok in re_words.findall(text_normalised) if not is_number_pattern.match(tok)]
+
+    tokens_without_stopwords = [tok for tok in tokens if tok not in stopwords_fr]
+
+    return tokens_without_stopwords
